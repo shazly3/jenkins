ea670b311c3492ae8867702c4f26d31810ea6c1a
Shazly <shazly mail>
2024-10-20 04:18:24 +0300

    changes

--- script.groovy
+++ script.groovy
@@ -8,7 +8,7 @@ def buildJar() {
 def buildImage() {
     echo "building the docker image..."
     withCredentials([usernamePassword(credentialsId: 'dockerhub-credentials', passwordVariable: 'PASSWORD', usernameVariable: 'USERNAME')]) {
-        sh 'docker build -t shazly3/jenkins:APP-2.0 .'
+        sh 'docker build -t shazly3/jenkins:latest .'
         sh "echo $PASSWORD | docker login -u $USERNAME --password-stdin"
         sh 'docker push shazly3/jenkins:latest'
     }
